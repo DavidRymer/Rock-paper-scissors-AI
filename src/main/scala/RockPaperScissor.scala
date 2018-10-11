ee3d91356a6d2e0a512cede7261e57a5c7281a7e
davidrymer <gotrees2012>
2018-10-11 11:35:14 +0100

    Ai vs Ai, after work on code reuse

--- src/main/scala/RockPaperScissor.scala
+++ src/main/scala/RockPaperScissor.scala
@@ -6,15 +6,16 @@ object RockPaperScissor {
     var rockCount = 0
     var paperCount = 0
     var scissorsCount = 0
-    var userWins = 0
-    var aiWins = 0
+    var player1Wins = 0
+    var player2Wins = 0
     var draws = 0
     var gameEnd = false
+    val options = Array("rock", "paper", "scissors")
 
 
 
     def artificialIntelligence(rockCount: Int, paperCount: Int, scissorCount: Int): String = {
-      val options = Array("rock", "paper", "scissors")
+
       var total = rockCount + scissorsCount + paperCount
 
       if (total%4 == 0 && total > 0)
@@ -26,45 +27,39 @@ object RockPaperScissor {
           case (a, b, c) if c > a && a >= b => "rock"
           case _ => Random.shuffle(options.toList).head
         }
-
-
-
-
     }
 
-    def result(userGuess: String, aiGuess: String): Unit = {
-      (userGuess, aiGuess) match {
+    def result(player1Choice: String, player2Choice: String): Unit = {
+      (player1Choice, player2Choice) match {
         case ("rock", "scissors") => rockCount += 1
-          userWins += 1
-          println("User wins \n")
+          player1Wins += 1
+          println("Player 1 wins \n")
         case ("paper", "rock") => paperCount += 1
-          userWins += 1
-          println("User wins \n")
+          player1Wins += 1
+          println("Player 1 wins \n")
         case ("scissors", "paper") => scissorsCount += 1
-          userWins += 1
-          println("User wins \n")
-        case ("rock", "rock") => rockCount += 1
-          draws += 1
+          player1Wins += 1
+          println("Player 1 wins \n")
+        case ("rock", "rock") => draws += 1
           println("Draw \n")
-        case ("paper", "paper") => paperCount += 1
-          draws += 1
+        case ("paper", "paper") => draws += 1
           println("Draw \n")
-        case ("scissors", "scissors") => scissorsCount += 1
-          draws += 1
+        case ("scissors", "scissors") => draws += 1
           println("Draw \n")
         case ("quit", _) => gameEnd = true
-        case _ => aiWins += 1
-          println("AI wins \n")
+        case _ => player2Wins += 1
+          println("Player 2 wins \n")
       }
 
     }
-
       def game(): Unit = {
         var userGuess = ""
         var aiGuess = ""
 
         while (!gameEnd) {
-          println(s"User wins: ${userWins}, AI wins: ${aiWins}, Draws: ${draws}")
+          if (player1Wins + player1Wins > 0)
+            println(s"User wins: $player1Wins, AI wins: $player2Wins, Draws: $draws")
+
           println("User please choose: rock, paper or scissors")
           userGuess = scala.io.StdIn.readLine()
 
@@ -76,19 +71,58 @@ object RockPaperScissor {
             println("Thank you for playing.")
           }
           result(userGuess, aiGuess)
-
         }
-
       }
 
+    def aiVsAi(player1Weight: Int): Unit = {
+      var player1Choice = ""
+      var player2Choice = ""
+
+      println(s"Welcome to the AI vs AI Rock, Paper," +
+        s" Scissors simulation. You have selected AI1 to" +
+        s" have an $player1Weight% favour.")
+
+      println("Please Type \"next\" to begin and again for each iteration")
+      while (!gameEnd) {
+        scala.io.StdIn.readLine() match {
+
+          case "next" => player2Choice = randomChoice()
+            if (Random.nextInt(100) < player1Weight - 10) {
+              player1Choice = win(player2Choice)
+            }
+            else {
+              player1Choice = randomChoice()
+            }
+            println(s"Player 1 chooses $player1Choice and player 2 chooses $player2Choice")
+            result(player1Choice, player2Choice)
+            if (player1Wins + player1Wins > 0)
+              println(s"AI1 wins: $player1Wins, AI2 wins: $player2Wins, Draws: $draws")
+
+          case "quit" => gameEnd = true
+            println ("Thank you for playing")
+
+          case _ => println("Please type in \"next\" or \"quit\".")
+        }
+      }
+    }
+    def win(opponentChoice: String): String = {
+      opponentChoice match {
+        case "rock" => "paper"
+        case "paper" => "scissors"
+        case "scissors"=> "rock"
+      }
+    }
+    def randomChoice():String = {
+      val options = Array("rock", "paper", "scissors")
+      Random.shuffle(options.toList).head
+
+    }
     def resetCounters(): Unit = {
       paperCount = 0
       scissorsCount = 0
       rockCount = 0
     }
-
-    game()
-
+    aiVsAi(80)
 
   }
 
